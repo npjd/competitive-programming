bc432e69c99fd283579b2c15a20e480979ba51fd
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-05-17 20:15:24 -0400

    math question

---
+++ CCC/penniesInTheRing.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int radius = 0;
+    cin>>radius;
+    while (radius!=0){
+        int result = 0;
+        for (int i = 1; i<=radius; i++)
+        {
+            for (int j = 0; j <= radius; j++)
+            {
+                if (i*i +j*j <= radius*radius)
+                {
+                    result+=1;
+                }else
+                {
+                    break;
+                }
+            }            
+        }
+        cout<<result<<endl;
+        cin>>radius;        
+    }
+
+}
