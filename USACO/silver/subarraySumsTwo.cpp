8938909b0ca389379c36006cd0b0f93fc904bfbc
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-11 23:39:27 -0400

    pre easy question ngl

--- USACO/silver/subarraySumsTwo.cpp
+++ USACO/silver/subarraySumsTwo.cpp
@@ -5,12 +5,26 @@ using namespace std;
 int main(){
     int n;
     int x;
+    int ans = 0;
     cin>>n>>x;
     int a[n];
     for (int i = 0; i < n; i++)
     {
         cin>>a[i];
     }
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += a[j];
+            if (sum == x)
+            {
+                ans+=1;                
+            }            
+        }        
+    }
+    cout<< ans<< endl;
     
     
 }
