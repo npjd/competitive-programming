260ee8483262ba98e13e53d20c223c05f782a380
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-14 19:08:38 -0400

    ez question too

--- USACO/silver/subArrayDivisbiilty.cpp
+++ USACO/silver/subArrayDivisbiilty.cpp
@@ -5,4 +5,27 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    int a[n];
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum +=a[j];
+            if (sum%n==0)
+            {
+                ans+=1;
+            }
+            
+        }        
+    }
+
+    cout<<ans<<endl;
+    
+    
 }
