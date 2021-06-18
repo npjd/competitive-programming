88bd9e35895efb109288b1ae5db98e9dd2e86598
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-18 14:40:30 -0400

    init

---
+++ USACO/silver/whydidthecowcrosstheroad.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int k;
+    int b;
+    cin>>n>>k>>b;
+    int signals[n];
+    int prefix[n +1];
+    prefix[0] = 0;
+    for (int i = 0; i < n; i++)
+    {
+        signals[i] = i+1;
+    }
+    
+    
+}
