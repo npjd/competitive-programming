b565003f056df716997b6453b8d7149ca048b788
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-18 14:51:04 -0400

    almost there

--- USACO/silver/whydidthecowcrosstheroad.cpp
+++ USACO/silver/whydidthecowcrosstheroad.cpp
@@ -6,15 +6,15 @@ int main(){
     int n;
     int k;
     int b;
-    int ans = 1000000000000000000000000000;
     cin>>n>>k>>b;
+    int ans = k;
     int signals[n+1];
     int prefix[n+1];
     for (int i = 0; i < b; i++)
     {
         int broke;
         cin>>broke;
-        signals[broke] += 1;
+        signals[broke] = 1;
     }
     for (int i = 1; i <= n; i++)
     {
