0951de3379f0fd49e224fa70c7f3f7714c654358
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-19 22:13:44 -0400

    elson was watching me ❤️😘

---
+++ CCC/ragaman.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int wrongCounter = 0;
+    int asteriskCounter = 0;
+    string str1;
+    string str2;
+    cin>>str1;
+    cin>>str2;
+
+    int n = str1.length();
+    
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());    
+
+    for (int i = 0; i < n; i++)
+    {
+        if (str1[i]=='*')
+        {
+            asteriskCounter++;
+            continue;
+        }
+        if (str2[i]=='*')
+        {
+            asteriskCounter++;
+            continue;
+        }
+        if (str1[i]!=str2[i])
+        {
+            wrongCounter++;
+        }                        
+    }
+
+
+    if (asteriskCounter >= wrongCounter)
+    {
+        cout<<"A"<<endl;
+    }else
+    {
+        cout<<"N"<<endl;
+    }
+    
+}
