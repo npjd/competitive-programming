b509dc65d2dfb0bc41f716be3b26b6563ff510fd
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-26 11:30:21 -0400

    mm

--- CCC/gates.cpp
+++ CCC/gates.cpp
@@ -5,6 +5,45 @@ using namespace std;
 int main(){
     int g;
     int p;
+    
     cin>>g;
     cin>>p;
+
+    int gates[g];
+    int planes[p];
+
+    for (int i = 0; i < p; i++)
+    {
+        cin>>planes[i];
+        int plane = planes[i];
+        while (plane!=0)
+        {
+            if (gates[plane]==0)
+            {
+                gates[plane]=1;
+                plane = 0;
+            }
+            else
+            {
+                plane--;
+                if (plane==0)
+                {
+                    i = p;
+                }                
+            }
+        }
+        int sum = 0;
+        gates[0]=-1;
+        for (int i = 0; i < g; i++)
+        {
+            if (gates[i]==1)
+            {
+                sum +=1;
+            }            
+        }
+        cout<<sum<<endl;
+        
+    }
+    
+    
 }
