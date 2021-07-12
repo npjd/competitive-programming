d0d82565efe8c4dccc02d24c1cb9c161008be250
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-07-12 19:35:11 -0400

    JUST SOME LIL PRACTICE

--- CCC/jerseys.cpp
+++ CCC/jerseys.cpp
@@ -3,41 +3,59 @@
 using namespace std;
 
 int main(){
-    int jerseys[100001];
+    int jerseys[1000001];
     int j;
     int a;
+    int ans =0;
+
     cin>>j;
     cin>>a;
-    for (int i = 1; i <= j; i++)
+    for (int i = 0; i < j; i++)
     {
         char c;
         cin>>c;
-        if(c=='S')
-            jerseys[i]=0;
-        else if(c=='M')
-            jerseys[i]=1;
-        else if(c=='L')
-            jerseys[i]=2;
-    }
-    int ans = 0;
-    for (int i = 1; i <= a; i++)
-    {
-        int num;
-        char size;
-        cin>>size>>num;
-        int s;
-        if(size=='S')
-            s=0;
-        else if(size=='M')
-            s=1;
-        else if(size=='L')
-            s=2;
-        if (jerseys[num]>=s)
+        if (c=='L')
         {
-            ans++;
-            jerseys[num]=-1;
+            jerseys[i] = 3;
+        }
+        if (c=='M')
+        {
+            jerseys[i] = 2;
+        }
+        if (c=='S')
+        {
+            jerseys[i] = 1;
+        }
+    }
+
+    for (int i = 0; i < a; i++)
+    {
+        int index;
+        char c;
+        int targetSize;
+        cin>>c>>index;
+        if (c=='L')
+        {
+            targetSize = 3;
+        }
+        if (c=='M')
+        {
+            targetSize = 2;
+        }
+        if (c=='S')
+        {
+            targetSize = 1;
+        }
+        int size = jerseys[index-1];
+        if (size>=targetSize)
+        {
+            jerseys[index-1] = -1;
+            ans+=1;
         }        
     }
+
     cout<<ans<<endl;
+    
+    
     
 }
