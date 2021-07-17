20e88983144d0cf22d3103b7a582ad9402c1011e
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-07-16 20:06:27 -0400

    oops forgot an important part

--- CCC/punchy.py
+++ CCC/punchy.py
@@ -14,14 +14,49 @@ while True:
         else:
             print(b)
     elif card[0]=="3":
-        a = a+b
+        if card[1]=="A":
+            if card[2] =="B":
+                a = a+b
+            else:
+                a = a+a
+        else:
+            if card[2] =="B":
+                b = b+b
+            else:
+                b = b+a        
     elif card[0]=="4":
-        a = a*b
+        if card[1]=="A":
+            if card[2] =="B":
+                a = a*b
+            else:
+                a = a*a
+        else:
+            if card[2] =="B":
+                b = b*b
+            else:
+                b = b*a
     elif card[0]=="5":
-        a = a-b
+        if card[1]=="A":
+            if card[2] =="B":
+                a = a-b
+            else:
+                a = a-a
+        else:
+            if card[2] =="B":
+                b = b-b
+            else:
+                b = b-a        
     elif card[0]=="6":
-        a = a//b
+        if card[1]=="A":
+            if card[2] =="B":
+                a = int(a/b)
+            else:
+                a = int(a/a)
+        else:
+            if card[2] =="B":
+                b = int(b/b)
+            else:
+                b = int(b/a)
     elif card[0]=="7":
         break
     card = input().split(" ")
-
