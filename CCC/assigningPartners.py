ad0f470d2e40ec392b73261c396a04bf5f1cc2d4
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-08-02 12:42:37 -0400

    solve agane

--- CCC/assigningPartners.py
+++ CCC/assigningPartners.py
@@ -2,21 +2,21 @@ n = int(input())
 line1 = input().split(" ")
 line2 = input().split(" ")
 good = True
-partners = {}
-
+dict = {}
 for i in range(n):
     first_person = line1[i]
     second_person = line2[i]
     if first_person == second_person:
         good = False
         break
-    if first_person in partners and partners[first_person]!= second_person:
-        good = False
-        break
-    partners[first_person] = second_person
-    partners[second_person] = first_person
+    elif first_person in dict:
+        if dict[first_person] != second_person:
+            good = False
+            break
+    dict[first_person] = second_person
+    dict[second_person] = first_person
 
 if good:
-    print('good')
+    print("good")
 else:
-    print('bad')
+    print("bad")
