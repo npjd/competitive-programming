4d56225adce81d47e7213d6e93f9c3d5c9c30b5f
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-08-04 22:47:52 -0400

    anotha one

--- CCC/partyinvite.py
+++ CCC/partyinvite.py
@@ -4,18 +4,19 @@ people = []
 for i in range(k):
     people.append(i+1)
 
+multiples = []
 
 for i in range(m):
-    multiple = int(input())
+    multiples.append(int(input()))
+
+for multiple in multiples: 
 
     filtered = []
 
     for i in range(len(people)):
         if (i+1) % multiple !=0:
             filtered.append(people[i])
-    
     people = filtered
-    
 
 for i in range(len(people)):
     print(people[i])
