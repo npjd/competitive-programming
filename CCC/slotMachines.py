cd611e62fe051f35fade8e6114eb186e2f4019ec
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-10-05 21:28:33 -0400

    fixed

--- CCC/slotMachines.py
+++ CCC/slotMachines.py
@@ -5,29 +5,19 @@ slot3 = int(input())
 plays = 0
 
 while quarters >0:
-    slot1+=1
-    quarters-=1
-    plays+=1
-    if slot1==35:
-        quarters+=30
-        slot1=0
-    if quarters == 0:
-        break
-    slot2+=1
-    quarters-=1
-    plays+=1
-    if slot2==100:
-        quarters+=100
-        slot2=0
-    if quarters == 0:
-        break
-    slot3+=1
-    quarters-=1
-    plays+=1
-    if slot3==10:
-        quarters+=9
-        slot3=0
-    if quarters == 0:
-        break
+    plays +=1
+    if plays % 3 == 0:
+        slot3+=1
+        if slot3%10==0:
+            quarters+=9
+    elif plays % 2 == 0:
+        slot2+=1
+        if slot2%100==0:
+            quarters+=60
+    else:
+        slot1+=1
+        if slot1%35==0:
+            quarters+=30
+    quarters -=1
 
 print("Martha plays "+str(plays)+" times before going broke.")
