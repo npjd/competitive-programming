7f397762c89a00c01ee70c813c16c5a6bdb4b6c0
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-01-28 13:17:14 -0500

    comment

--- CCC/friends.py
+++ CCC/friends.py
@@ -1,37 +1,46 @@
-n = int(input())
-friend = []
-visited = []
-for i in range(10000):
-    friend.append(0)
-# the friend array maps the index to which person that friend index is friends with
-for i in range(n):
-    integers = input().split()
-    # we do that here!!!
-    friend[int(integers[0])] = int(integers[1])
-# grab the two friends to compare
-friendInput = input().split()
-friend1 = int(friendInput[0])
-friend2 = int(friendInput[1])
+# BFS BABYY
+def bfs(f1,f2,graph):
+    # count distance
+    res = -1
+    # if the neighbour is the friend we're looking for, we stop at 0
+    curr = graph[f1]
+    if curr == f2:
+        return 0
+    res = 1
+    while True:
+        # find the neighbouring friend
+        curr = friends[curr]
+        # if it's the friend we're looking for, we retrun it
+        if curr == f2:
+            return res
+        # if we're back at the first friend, we're done
+        if curr == f1:
+            return -1
+        res += 1
 
-while friend1 != 0 and friend2 != 0:
-    # set all visited friends to false
-    for i in range(10000):
-        visited.append(False)
-    # init distance
-    distance = -1
-    # if we've already visited this node, we don't loop again.
-    # if both the friends are the same we quit the loop as well as to not repeat ourselves
-    while visited[friend1]==False and friend1!=friend2:
-        # set the current node as visited
-        visited[friend1] = True
-        distance += 1
-        # set the next node we're visiting to the current node's friend
-        friend1 = friend[friend1]
-    
-    if friend1 ==friend2:
-        print("Yes",distance)
+# takes in number of friend pairs
+n = int(input())
+# init friend array
+friends = [0 for _ in range(10000)]
+
+# add friend combos to graph
+for i in range(n):
+    f1,f2 = input().split(' ')
+    f1 = int(f1)
+    f2 = int(f2)
+    friends[f1] = f2
+
+f1,f2 = input().split(' ')
+f1 = int(f1)
+f2 = int(f2)
+
+while f1 != 0 or f2 != 0:
+    # do bfs
+    res = bfs(f1,f2,friends)
+    if res == -1:
+        print("No")
     else:
-        print("No")    
-    friendInput = input().split()
-    friend1 = int(friendInput[0])
-    friend2 = int(friendInput[1])
+        print("Yes",res)
+    f1,f2 = input().split(' ')
+    f1 = int(f1)
+    f2 = int(f2)
