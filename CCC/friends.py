5a1c0c87e22b35e9f8c731831db9459fd5f9eab6
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-01-28 12:46:35 -0500

    more commenting

--- CCC/friends.py
+++ CCC/friends.py
@@ -3,23 +3,29 @@ friend = []
 visited = []
 for i in range(10000):
     friend.append(0)
-
+# the friend array maps the index to which person that friend index is friends with
 for i in range(n):
     integers = input().split()
+    # we do that here!!!
     friend[int(integers[0])] = int(integers[1])
-
+# grab the two friends to compare
 friendInput = input().split()
 friend1 = int(friendInput[0])
 friend2 = int(friendInput[1])
 
 while friend1 != 0 and friend2 != 0:
+    # set all visited friends to false
     for i in range(10000):
         visited.append(False)
+    # init distance
     distance = -1
-
+    # if we've already visited this node, we don't loop again.
+    # if both the friends are the same we quit the loop as well as to not repeat ourselves
     while visited[friend1]==False and friend1!=friend2:
+        # set the current node as visited
         visited[friend1] = True
         distance += 1
+        # set the next node we're visiting to the current node's friend
         friend1 = friend[friend1]
     
     if friend1 ==friend2:
@@ -29,4 +35,3 @@ while friend1 != 0 and friend2 != 0:
     friendInput = input().split()
     friend1 = int(friendInput[0])
     friend2 = int(friendInput[1])
-        
--- CCC/mouse.py
+++ CCC/mouse.py
@@ -9,17 +9,17 @@ dx,dy = input().split(' ')
 dx = int(dx)
 dy = int(dy)
 
-while(dx!=0 or dy!=0):
-    x+=dx
-    y+=dy
-    if x >c:
+while (dx != 0 or dy!=0):
+    x += dx
+    y += dy
+    if x>c:
         x=c
     if x<0:
         x=0
     if y > r:
-        y=r
-    if y<0:
-        y=0
+        y = r
+    if y < 0:
+        y = 0
     print(x,y)
     dx,dy = input().split(' ')
     dx = int(dx)
