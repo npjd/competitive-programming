2c6ce90a4b8f6411d6a10898c6042e4cc61ef603
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-03-31 14:38:20 -0400

    commented code

--- CCC/billsBonucalars.py
+++ CCC/billsBonucalars.py
@@ -1,22 +1,32 @@
+# get input
 n, k = input().split()
 n = int(n)
 k = int(k)
+# count for min houses needed
 count =0
+# tracking people in each house
 houses = [0 for _ in range(n+1)]
+# final array
 res = [0 for _ in range(n+1)]
 
-
+# get the input
 for _ in range(k):
     a,b,x = input().split()
     a = int(a)
     b = int(b)
     x = int(x)
+    # simulate people leaving house
     houses[a] -=x 
+    # simulate people joining house
     houses[b] +=x
+    # check if the house needs more members to join and add it in our final array
     res[a] = min(res[a],houses[a])
 
 for i in range(1,n+1):
+    # increment the count
     count += -res[i]
+
+# print all results
 
 print(count)
 
